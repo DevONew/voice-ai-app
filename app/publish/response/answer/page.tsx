41c78c092b5f61b5a5419aa8240b1f9375b67642
DevONew <kyl2778@naver.com>
2025-12-11 23:01:59 +0900

    Revert "refactor: publish 페이지 삭제 및 콘솔 로그 제거"
    This reverts commit 1f4ba81e7ee6118162b529f68ad409d39fc42279.

---
+++ app/publish/response/answer/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState } from 'react'
+import VoiceButton from '../../../components/VoiceButton'
+import StateTextDisplay from '../../../components/StateTextDisplay'
+
+const answerText = '설정 완료 되었습니다.'
+
+export default function PublishResponseAnswerPage() {
+  const [isListening, setIsListening] = useState(false)
+
+  const handleButtonClick = () => {
+    setIsListening(!isListening)
+  }
+
+  return (
+    <div className="w-full h-screen bg-white flex flex-col items-center p-4 relative">
+      {/* 상단 텍스트 */}
+      <StateTextDisplay text={answerText} />
+
+      {/* 하단 원 */}
+      <div className="absolute left-1/2 transform -translate-x-1/2" style={{ bottom: '40px' }}>
+        <VoiceButton
+          isAnimating={true}
+          scale={0.4}
+          isListening={isListening}
+          onClick={handleButtonClick}
+        />
+      </div>
+    </div>
+  )
+}
